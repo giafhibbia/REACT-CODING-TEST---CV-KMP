c8bb5c5cd5284e5fcbb02b7cc31d945f28cbdaf3
zaenal muttaqien <zaenalmuttaqien3@gmail.com>
2020-04-23 16:15:40 +0700

    for page home gallery

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route } from "react-router-dom";
+import HomePage from './HomePage';
+import { createBrowserHistory as createHistory } from 'history'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
-
+import ImageSearchPage from './ImageSearchPage';
+import VideoSearchPage from './VideoSearchPage';
+const history = createHistory();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <Navbar bg="primary" expand="lg" variant="dark" >
+          <Navbar.Brand href="/">Image Gallery App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/imagesearch" exact component={ImageSearchPage} />
+        <Route path="/videosearch" exact component={VideoSearchPage} />
+      </Router>
     </div>
   );
 }
-
 export default App;
